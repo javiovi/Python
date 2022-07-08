cbd22c8f6195c389cc796c4775562b5ec5afcca0
javiovi <106032666+javiovi@users.noreply.github.com>
2022-07-08 17:32:58 -0300

    Ejercicio modulo 5 open camp

---
+++ 5-2.py
@@ -0,0 +1,14 @@
+def numeroprimo():
+    numero: int = int(input('Introduce un número entero: '))
+
+    if numero > 1:
+        for i in range(2,int(numero)):
+            if (int(numero) % i) == 0:
+                print(f"Lo siento, el número {numero} NO ES PRIMO. Es divisible entre {i}")
+                break
+            else:
+                print(f"el número {numero} ES PRIMO!")
+    else:
+        print(f"Lo siento, el número {numero} NO ES PRIMO. Los números primos son mayores que 1")
+
+print({numeroprimo()})
