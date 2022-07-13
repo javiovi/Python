e4535ba7e51e8cdb31713ebfcbc3c4906a38cb44
javiovi <106032666+javiovi@users.noreply.github.com>
2022-07-13 18:16:14 -0300

    Modulo7-Modulos

---
+++ Modulo7.py
@@ -0,0 +1,8 @@
+from Operaciones import *
+
+a, b, c, d =(2, 3, 6, 8)
+
+print( "{} + {} = {}".format(a, b, suma(a, b) ) )
+print( "{} - {} = {}".format(b, c, resta(b, c)))
+print( "{} * {} = {}".format(a, d, multiplicar(a, d)))
+print( "{} / {} = {}".format(d, a, division(d, a)))
