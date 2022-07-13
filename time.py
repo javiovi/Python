76e7ea82410c9294ce75a3fa346bce8b3d0c9c8c
javiovi <106032666+javiovi@users.noreply.github.com>
2022-07-13 18:22:45 -0300

    Ejercicio-time-modulo7

---
+++ time.py
@@ -0,0 +1,12 @@
+import time
+
+hora = time.strftime('%H')
+minutos = time.strftime('%M')
+
+
+if int(hora) >= 19:
+    print ("Ya es hora de irse a casa")
+
+else:
+    print("Quedan {} horas y {} minutos para irse a casa".format(18- int(hora), 59-int(minutos)))
+
